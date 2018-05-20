6a8e987c4e1b2d46f0e24407b6527df4e08c74e0
_c0da_ <duvictor514@gmail.com>
2018-05-19 21:41:58 -0700

    Added placeholder menu bar

--- app/src/main/java/com/team3543/trcattendance_android/MainActivity.java
+++ app/src/main/java/com/team3543/trcattendance_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.team3543.trcattendance_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,4 +15,25 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_fileio, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_test:
+            {
+                break;
+            }
+        }
+        return false;
+    }
+
 }
