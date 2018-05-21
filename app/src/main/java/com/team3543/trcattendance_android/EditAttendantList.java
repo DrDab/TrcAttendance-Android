a8d46ca0037170f8f035f16f6ebfaf3eb34ead52
_c0da_ <duvictor514@gmail.com>
2018-05-21 14:58:12 -0700

    Added stubs for list of attendants

---
+++ app/src/main/java/com/team3543/trcattendance_android/EditAttendantList.java
@@ -0,0 +1,15 @@
+package com.team3543.trcattendance_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAttendantList extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_attendant_list);
+    }
+}
